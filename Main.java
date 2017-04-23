f9a5fd32a7284d82290c142113cf5f786f7a0b89
lietuviskalba <mantasling@gmail.com>
2017-04-23 18:12:03 +0200

    Add a main dot java
    its the first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //ty lfor the suggestion
+        //you are the best
+        //I know it ....
+    }
+}
